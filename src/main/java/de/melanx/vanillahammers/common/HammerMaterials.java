ded7b28958ffa4cadf129cb2403eb8aa7799a727
MelanX <meissi97@me.com>
2020-01-21 20:17:19 +0100

    changed way of registration

--- src/main/java/de/melanx/vanillahammers/common/HammerMaterials.java
+++ src/main/java/de/melanx/vanillahammers/common/HammerMaterials.java
@@ -5,74 +5,164 @@ import net.minecraft.block.Blocks;
 import net.minecraft.item.IItemTier;
 import net.minecraft.item.Items;
 import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.Tag;
+import net.minecraft.util.IItemProvider;
 import net.minecraft.util.LazyLoadBase;
+import net.minecraftforge.common.Tags;
 
 import java.util.function.Supplier;
 
 public enum HammerMaterials implements IItemTier {
-    WOOD(0, 59 * VanillaHammers.DURABILITY_MODIFIER, 2.0F / 3.5f, 0.0f, 15, () -> {
+    WOOD(0, 59 * VanillaHammers.DURABILITY_MODIFIER, 2.0F / 3.5f, 3, -2.5F, 15, () -> {
         return Ingredient.fromTag(ItemTags.PLANKS);
-    }),
-    STONE(1, 131 * VanillaHammers.DURABILITY_MODIFIER, 4.0F / 3.5f, 0.0f, 5, () -> {
+    }, "wooden", BlockTags.PLANKS, BlockTags.LOGS),
+    STONE(1, 131 * VanillaHammers.DURABILITY_MODIFIER, 4.0F / 3.5f, 4, -2.6F, 5, () -> {
         return Ingredient.fromItems(Blocks.COBBLESTONE);
-    }),
-    IRON(2, 250 * VanillaHammers.DURABILITY_MODIFIER, 6.0F / 3.5f, 0.0f, 14, () -> {
+    }, "stone", Tags.Blocks.COBBLESTONE, Tags.Blocks.STONE),
+    IRON(2, 250 * VanillaHammers.DURABILITY_MODIFIER, 6.0F / 3.5f, 5, -2.8F, 14, () -> {
         return Ingredient.fromItems(Items.IRON_INGOT);
-    }),
-    DIAMOND(3, 1561 * VanillaHammers.DURABILITY_MODIFIER, 8.0F / 3.5f, 0.0f, 10, () -> {
-        return Ingredient.fromItems(Items.DIAMOND);
-    }),
-    GOLD(0, 32 * VanillaHammers.DURABILITY_MODIFIER, 12.0F / 3.5f, 0.0f, 22, () -> {
+    }, "iron", Tags.Items.INGOTS_IRON, Tags.Blocks.STORAGE_BLOCKS_IRON),
+    GOLD(0, 32 * VanillaHammers.DURABILITY_MODIFIER, 12.0F / 3.5f, 5, -2.5F, 22, () -> {
         return Ingredient.fromItems(Items.GOLD_INGOT);
-    }),
-    EMERALD(3, 1028 * VanillaHammers.DURABILITY_MODIFIER, 12.0F / 3.5f, 0.0f, 25, () -> {
+    }, "golden", Tags.Items.INGOTS_GOLD, Tags.Blocks.STORAGE_BLOCKS_GOLD),
+    DIAMOND(3, 1561 * VanillaHammers.DURABILITY_MODIFIER, 8.0F / 3.5f, 10, -3.0F, 10, () -> {
+        return Ingredient.fromItems(Items.DIAMOND);
+    }, "diamond", Tags.Items.GEMS_DIAMOND, Tags.Blocks.STORAGE_BLOCKS_DIAMOND),
+    EMERALD(3, 1028 * VanillaHammers.DURABILITY_MODIFIER, 12.0F / 3.5f, 11, -3.0F, 25, () -> {
         return Ingredient.fromItems(Items.EMERALD);
-    }),
-    OBSIDIAN(2, 2048 * VanillaHammers.DURABILITY_MODIFIER, 5.0F / 3.5f, 0.0f, 5, () -> {
+    }, "emerald", Tags.Items.GEMS_EMERALD, Tags.Blocks.STORAGE_BLOCKS_EMERALD),
+    OBSIDIAN(2, 2048 * VanillaHammers.DURABILITY_MODIFIER, 5.0F / 3.5f, 7, -3.5F, 5, () -> {
         return Ingredient.fromItems(Items.OBSIDIAN);
-    }),
-    LAPIS(1, 220 * VanillaHammers.DURABILITY_MODIFIER, 6.0f / 3.5f, 0.0f, 30, () -> {
-        return Ingredient.fromItems(Items.LAPIS_LAZULI);
-    }),
-    QUARTZ(2, 1028 * VanillaHammers.DURABILITY_MODIFIER, 8.0F / 3.5f, 0.0f, 10, () -> {
+    }, "obsidian", Tags.Blocks.OBSIDIAN),
+    QUARTZ(2, 1028 * VanillaHammers.DURABILITY_MODIFIER, 8.0F / 3.5f, 5, -2.0F, 10, () -> {
         return Ingredient.fromItems(Items.QUARTZ);
-    }),
-    FIERY(3, 750 * VanillaHammers.DURABILITY_MODIFIER, 7.0F / 3.5f, 0f, 15, () -> {
+    }, "quartz", Tags.Items.GEMS_QUARTZ, Tags.Blocks.STORAGE_BLOCKS_QUARTZ),
+    LAPIS(1, 220 * VanillaHammers.DURABILITY_MODIFIER, 6.0f / 3.5f, 3, -2.5F, 30, () -> {
+        return Ingredient.fromItems(Items.LAPIS_LAZULI);
+    }, "lapis", Tags.Items.GEMS_LAPIS, Tags.Blocks.STORAGE_BLOCKS_LAPIS),
+    FIERY(3, 750 * VanillaHammers.DURABILITY_MODIFIER, 7.0F / 3.5f, 6, -2.3F, 15, () -> {
         return Ingredient.fromItems(Items.MAGMA_BLOCK);
-    }),
-    PRISMARINE(3, 750 * VanillaHammers.DURABILITY_MODIFIER, 7.0F / 3.5F, 0f, 20, () -> {
+    }, "fiery", Items.MAGMA_CREAM, Blocks.MAGMA_BLOCK),
+    PRISMARINE(3, 750 * VanillaHammers.DURABILITY_MODIFIER, 7.0F / 3.5F, 6, -2.3F, 20, () -> {
         return Ingredient.fromItems(Items.PRISMARINE_SHARD);
-    }),
-    ENDER(3, 1561 * VanillaHammers.DURABILITY_MODIFIER, 10f / 3.5f, 0, 10, () -> {
+    }, "prismarine", Tags.Items.DUSTS_PRISMARINE, Blocks.PRISMARINE),
+    ENDER(3, 1561 * VanillaHammers.DURABILITY_MODIFIER, 10f / 3.5f, 10, -3.3F, 10, () -> {
         return Ingredient.fromItems(Items.ENDER_PEARL);
-    }),
-    SLIME(2, 1500 * VanillaHammers.DURABILITY_MODIFIER, 6f / 3.5f, 0, 20, () -> {
+    }, "ender", Items.DRAGON_HEAD, Tags.Blocks.END_STONES),
+    SLIME(2, 1500 * VanillaHammers.DURABILITY_MODIFIER, 6f / 3.5f, 7, -3.0F, 20, () -> {
         return Ingredient.fromItems(Items.SLIME_BALL);
-    }),
-    GLOWSTONE(2, 189 * VanillaHammers.DURABILITY_MODIFIER, 5.0F / 3.5f, 0.0f, 15, () -> {
+    }, "slime", Tags.Items.SLIMEBALLS, Blocks.SLIME_BLOCK),
+    GLOWSTONE(2, 189 * VanillaHammers.DURABILITY_MODIFIER, 5.0F / 3.5f, 5, -2.3F, 15, () -> {
         return Ingredient.fromItems(Items.GLOWSTONE);
-    }),
-    NETHER(1, 280 * VanillaHammers.DURABILITY_MODIFIER, 5.0F / 3.5f, 0.0f, 77, () -> {
+    }, "glowstone", Tags.Items.DUSTS_GLOWSTONE, Blocks.GLOWSTONE),
+    NETHER(1, 280 * VanillaHammers.DURABILITY_MODIFIER, 5.0F / 3.5f, 4, -2.1F, 77, () -> {
         return Ingredient.fromItems(Items.NETHER_BRICKS);
-    });
+    }, "nether", Tags.Blocks.NETHERRACK, Blocks.NETHER_BRICKS);
 
     private final int harvestLevel;
     private final int durability;
     private final float efficiency;
     private final float attackDamage;
+    private final float attackSpeed;
     private final int enchantability;
     private final LazyLoadBase<Ingredient> repairMaterial;
+    private final String prefix;
+    private final Tag<?> tagIngredient1;
+    private final Tag<?> tagIngredient2;
+    private final IItemProvider itemIngredient1;
+    private final IItemProvider itemIngredient2;
 
-    HammerMaterials(int harvestLevel, int durability, float efficiency, float attackDamage, int enchantability, Supplier<Ingredient> repairMaterial) {
+    HammerMaterials(int harvestLevel, int durability, float efficiency, float attackDamage, float attackSpeed, int enchantability, Supplier<Ingredient> repairMaterial, String prefix, Tag<?> ingredient1, Tag<?> ingredient2) {
         this.harvestLevel = harvestLevel;
         this.durability = durability;
         this.efficiency = efficiency;
         this.attackDamage = attackDamage;
+        this.attackSpeed = attackSpeed;
         this.enchantability = enchantability;
         this.repairMaterial = new LazyLoadBase<>(repairMaterial);
+        this.prefix = prefix;
+        this.itemIngredient1 = null;
+        this.itemIngredient2 = null;
+        this.tagIngredient1 = ingredient1;
+        this.tagIngredient2 = ingredient2;
     }
 
+    HammerMaterials(int harvestLevel, int durability, float efficiency, float attackDamage, float attackSpeed, int enchantability, Supplier<Ingredient> repairMaterial, String prefix, IItemProvider ingredient1, Tag<?> ingredient2) {
+        this.harvestLevel = harvestLevel;
+        this.durability = durability;
+        this.efficiency = efficiency;
+        this.attackDamage = attackDamage;
+        this.attackSpeed = attackSpeed;
+        this.enchantability = enchantability;
+        this.repairMaterial = new LazyLoadBase<>(repairMaterial);
+        this.prefix = prefix;
+        this.itemIngredient1 = ingredient1;
+        this.itemIngredient2 = null;
+        this.tagIngredient1 = null;
+        this.tagIngredient2 = ingredient2;
+    }
+
+    HammerMaterials(int harvestLevel, int durability, float efficiency, float attackDamage, float attackSpeed, int enchantability, Supplier<Ingredient> repairMaterial, String prefix, Tag<?> ingredient1, IItemProvider ingredient2) {
+        this.harvestLevel = harvestLevel;
+        this.durability = durability;
+        this.efficiency = efficiency;
+        this.attackDamage = attackDamage;
+        this.attackSpeed = attackSpeed;
+        this.enchantability = enchantability;
+        this.repairMaterial = new LazyLoadBase<>(repairMaterial);
+        this.prefix = prefix;
+        this.itemIngredient1 = null;
+        this.itemIngredient2 = ingredient2;
+        this.tagIngredient1 = ingredient1;
+        this.tagIngredient2 = null;
+    }
+
+    HammerMaterials(int harvestLevel, int durability, float efficiency, float attackDamage, float attackSpeed, int enchantability, Supplier<Ingredient> repairMaterial, String prefix, IItemProvider ingredient1, IItemProvider ingredient2) {
+        this.harvestLevel = harvestLevel;
+        this.durability = durability;
+        this.efficiency = efficiency;
+        this.attackDamage = attackDamage;
+        this.attackSpeed = attackSpeed;
+        this.enchantability = enchantability;
+        this.repairMaterial = new LazyLoadBase<>(repairMaterial);
+        this.prefix = prefix;
+        this.itemIngredient1 = ingredient1;
+        this.itemIngredient2 = ingredient2;
+        this.tagIngredient1 = null;
+        this.tagIngredient2 = null;
+    }
+
+    HammerMaterials(int harvestLevel, int durability, float efficiency, float attackDamage, float attackSpeed, int enchantability, Supplier<Ingredient> repairMaterial, String prefix, IItemProvider ingredient1) {
+        this.harvestLevel = harvestLevel;
+        this.durability = durability;
+        this.efficiency = efficiency;
+        this.attackDamage = attackDamage;
+        this.attackSpeed = attackSpeed;
+        this.enchantability = enchantability;
+        this.repairMaterial = new LazyLoadBase<>(repairMaterial);
+        this.prefix = prefix;
+        this.itemIngredient1 = ingredient1;
+        this.itemIngredient2 = null;
+        this.tagIngredient1 = null;
+        this.tagIngredient2 = null;
+    }
+
+    HammerMaterials(int harvestLevel, int durability, float efficiency, float attackDamage, float attackSpeed, int enchantability, Supplier<Ingredient> repairMaterial, String prefix, Tag<?> ingredient1) {
+        this.harvestLevel = harvestLevel;
+        this.durability = durability;
+        this.efficiency = efficiency;
+        this.attackDamage = attackDamage;
+        this.attackSpeed = attackSpeed;
+        this.enchantability = enchantability;
+        this.repairMaterial = new LazyLoadBase<>(repairMaterial);
+        this.prefix = prefix;
+        this.itemIngredient1 = null;
+        this.itemIngredient2 = null;
+        this.tagIngredient1 = ingredient1;
+        this.tagIngredient2 = null;
+    }
 
     @Override
     public int getMaxUses() {
@@ -101,5 +191,28 @@ public enum HammerMaterials implements IItemTier {
         return this.repairMaterial.getValue();
     }
 
+    public String getPrefix() {
+        return this.prefix;
+    }
+
+    public IItemProvider getItemIngredient1() {
+        return this.itemIngredient1;
+    }
+
+    public IItemProvider getItemIngredient2() {
+        return this.itemIngredient2;
+    }
+
+    public Tag<?> getTagIngredient1() {
+        return this.tagIngredient1;
+    }
+
+    public Tag<?> getTagIngredient2() {
+        return this.tagIngredient2;
+    }
+
+    public float getAttackSpeed() {
+        return this.attackSpeed;
+    }
 
 }
--- src/main/java/de/melanx/vanillahammers/common/HammerRegistry.java
+++ src/main/java/de/melanx/vanillahammers/common/HammerRegistry.java
@@ -2,34 +2,28 @@ package de.melanx.vanillahammers.common;
 
 import de.melanx.vanillahammers.VanillaHammers;
 import de.melanx.vanillahammers.api.HammerItem;
-import de.melanx.vanillahammers.config.VanillaHammersConfig;
-import net.minecraft.item.IItemTier;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.registry.Registry;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class HammerRegistry {
-    public static void registerHammers() {
-        register(HammerMaterials.WOOD, 3, -2.5f, "wooden");
-        register(HammerMaterials.STONE, 4, -2.6f, "stone");
-        register(HammerMaterials.IRON, 5, -2.8f, "iron");
-        register(HammerMaterials.GOLD, 5, -2.5f, "golden");
-        register(HammerMaterials.DIAMOND, 10, -3f, "diamond");
 
-        if (VanillaHammersConfig.extraHammers.get()) {
-            register(HammerMaterials.EMERALD, 11, -3f, "emerald");
-            register(HammerMaterials.OBSIDIAN, 7, -3.5f, "obsidian");
-            register(HammerMaterials.QUARTZ, 5, -2f, "lapis");
-            register(HammerMaterials.LAPIS, 3, -2.5f, "quartz");
-            register(HammerMaterials.FIERY, 6, -2.3f, "fiery");
-            register(HammerMaterials.PRISMARINE, 6, -2.3f, "prismarine");
-            register(HammerMaterials.ENDER, 10, -3.3f, "ender");
-            register(HammerMaterials.SLIME, 7, -3f, "slime");
-            register(HammerMaterials.GLOWSTONE, 5, -2.3f, "glowstone");
-            register(HammerMaterials.NETHER, 4, -2.1f, "nether");
+    public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, VanillaHammers.MODID);
+
+    public static void registerHammers() {
+
+        for (HammerMaterials type : HammerMaterials.values()) {
+            register(type);
         }
+
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 
-    private static HammerItem register(IItemTier material, int attackDamage, float attackSpeed, String hammerName) {
-        return Registry.register(Registry.ITEM, new ResourceLocation(VanillaHammers.MODID, hammerName + "_hammer"), new HammerItem(material, attackDamage, attackSpeed));
+    private static void register(HammerMaterials type) {
+        String name = type.getPrefix();
+        int attackDamage = (int) type.getAttackDamage();
+        float attackSpeed = type.getAttackSpeed();
+        ITEMS.register(name + "_hammer", () -> new HammerItem(type, attackDamage, attackSpeed));
     }
 }
