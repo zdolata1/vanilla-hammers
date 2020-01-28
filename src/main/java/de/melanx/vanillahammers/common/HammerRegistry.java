41d0973c96172f95c93c044dcbfe2621eac720ff
MelanX <meissi97@me.com>
2020-01-28 19:17:01 +0100

    simplified registration

--- src/main/java/de/melanx/vanillahammers/common/HammerRegistry.java
+++ src/main/java/de/melanx/vanillahammers/common/HammerRegistry.java
@@ -3,40 +3,20 @@ package de.melanx.vanillahammers.common;
 import de.melanx.vanillahammers.VanillaHammers;
 import de.melanx.vanillahammers.api.HammerItem;
 import net.minecraft.item.Item;
-import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-
-import static de.melanx.vanillahammers.common.HammerMaterials.*;
 
 public class HammerRegistry {
 
     public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, VanillaHammers.MODID);
 
-    public static final RegistryObject<Item> WOODEN_HAMMER = register(WOOD);
-    public static final RegistryObject<Item> STONE_HAMMER = register(STONE);
-    public static final RegistryObject<Item> IRON_HAMMER = register(IRON);
-    public static final RegistryObject<Item> GOLD_HAMMER = register(GOLD);
-    public static final RegistryObject<Item> DIAMOND_HAMMER = register(DIAMOND);
-    public static final RegistryObject<Item> EMERALD_HAMMER = register(EMERALD);
-    public static final RegistryObject<Item> OBSIDIAN_HAMMER = register(OBSIDIAN);
-    public static final RegistryObject<Item> QUARTZ_HAMMER = register(QUARTZ);
-    public static final RegistryObject<Item> LAPIS_HAMMER = register(LAPIS);
-    public static final RegistryObject<Item> FIERY_HAMMER = register(FIERY);
-    public static final RegistryObject<Item> PRISMARINE_HAMMER = register(PRISMARINE);
-    public static final RegistryObject<Item> ENDER_HAMMER = register(ENDER);
-    public static final RegistryObject<Item> SLIME_HAMMER = register(SLIME);
-    public static final RegistryObject<Item> GLOWSTONE_HAMMER = register(GLOWSTONE);
-    public static final RegistryObject<Item> NETHER_HAMMER = register(NETHER);
-
     public static void registerHammers() {
+        for (HammerMaterials type : HammerMaterials.values()) {
+            String name = type.getPrefix();
+            float attackSpeed = type.getAttackSpeed();
+            ITEMS.register(name + "_hammer", () -> new HammerItem(type, 0, attackSpeed));
+        }
         ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
-    }
-
-    private static RegistryObject<Item> register(HammerMaterials type) {
-        String name = type.getPrefix();
-        float attackSpeed = type.getAttackSpeed();
-        return ITEMS.register(name + "_hammer", () -> new HammerItem(type, 0, attackSpeed));
     }
 }
