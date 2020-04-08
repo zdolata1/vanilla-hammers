1d12a813ba692043b8ab9edf64ec49483324d7bd
MelanX <meissi97@me.com>
2020-04-08 13:58:55 +0200

    fixed a server crash with obsidian hammer

--- src/main/java/de/melanx/vanillahammers/items/ObsidianHammer.java
+++ src/main/java/de/melanx/vanillahammers/items/ObsidianHammer.java
@@ -3,8 +3,8 @@ package de.melanx.vanillahammers.items;
 import de.melanx.morevanillalib.api.BigBreakItem;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.item.IItemTier;
 import net.minecraft.item.ItemStack;
 
@@ -17,7 +17,7 @@ public class ObsidianHammer extends BigBreakItem {
     @Override
     public float getDestroySpeed(ItemStack stack, BlockState state) {
         if (state.getBlock() == Blocks.OBSIDIAN) {
-            int efficiencyLevel = EnchantmentHelper.getEnchantmentLevel(Enchantment.getEnchantmentByID(18), stack);
+            int efficiencyLevel = EnchantmentHelper.getEnchantmentLevel(Enchantments.EFFICIENCY, stack);
             return 5.0F * (efficiencyLevel / 3.5F + 1);
         } else {
             return super.getDestroySpeed(stack, state);
